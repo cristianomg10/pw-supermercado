daa275d298f8929c668ac0ca488e4304da1bedde
Cristiano Mesquita Garcia <cristiano.garcia@cacador.ifsc.edu.br>
2026-04-06 19:38:04 -0300

    >

--- resources/views/produtos_listar.blade.php
+++ resources/views/produtos_listar.blade.php
@@ -27,12 +27,12 @@
                 <a href="{{ route('prod.edit', ['id' => $p->id]) }}" 
                     class="btn btn-warning">
                     Alterar</a>
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalDelete{{ $p->id }}">
-                Launch demo modal
+                <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalDelete{{ $p->id }}">
+                Excluir
                 </button>
             </td>
         </tr>
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade" id="modalDelete{{ $p->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                 <div class="modal-header">
@@ -40,7 +40,7 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    ...
+                    Deseja realmente excluir o item <b>{{$p->nome}}</b>?
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
